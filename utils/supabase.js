9564e0e0a8587ef837b01e383e2b27e57c5854de
Manish6523 <220101005092@gandhinagaruni.ac.in>
2025-07-23 23:38:33 +0530

    added keys to .env

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://okcvvfslqgxoogyxevea.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rY3Z2ZnNscWd4b29neXhldmVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxMzQwNDcsImV4cCI6MjA2NTcxMDA0N30.aFePH2FArsPvokJXOfR2h-hOfUgElUrlU1hn7vRkqmM"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
